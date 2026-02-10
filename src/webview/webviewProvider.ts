e585c2dfbe29b96da026b13c774d7203ab6b6c02
rajeevreddy <rajeevreddy@gmail.com>
2026-02-10 22:28:21 +0530

    refactor: wire AGENT_INSTRUCTIONS as runtime fallback for instructionText
    Uses AGENT_INSTRUCTIONS constant as fallback when config value is empty,
    ensuring instructions are always available even if package.json default
    fails to load.

--- src/webview/webviewProvider.ts
+++ src/webview/webviewProvider.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 import { FILE_EXCLUSION_PATTERNS, FILE_SEARCH_EXCLUSION_PATTERNS, formatExcludePattern } from '../constants/fileExclusions';
+import { AGENT_INSTRUCTIONS } from '../constants/instructions';
 import { ContextManager, ContextReferenceType, ContextReference } from '../context';
 import { resolvePlanReview } from '../planReview/index';
 import { PlanReviewPanelResult } from '../planReview/types';
@@ -665,7 +666,7 @@ export class FlowCommandWebviewProvider implements vscode.WebviewViewProvider, v
         this._mobileNotificationEnabled = config.get<boolean>('mobileNotification', false);
         this._interactiveApprovalEnabled = config.get<boolean>('interactiveApproval', true);
         this._instructionInjection = config.get<string>('instructionInjection', 'off');
-        this._instructionText = config.get<string>('instructionText', '');
+        this._instructionText = config.get<string>('instructionText', '') || AGENT_INSTRUCTIONS;
 
         // Load reusable prompts from settings
         const savedPrompts = config.get<Array<{ name: string; prompt: string; isTemplate?: boolean }>>('reusablePrompts', []);
